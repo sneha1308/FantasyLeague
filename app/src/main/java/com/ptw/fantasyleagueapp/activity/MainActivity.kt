dabbe85d085882903bd0f4e432941268dd311158
sridharG-PTW <sridhar.gonda@ptw.com>
2020-02-03 12:16:47 +0530

    added about screen button ui

--- app/src/main/java/com/ptw/fantasyleagueapp/activity/MainActivity.kt
+++ app/src/main/java/com/ptw/fantasyleagueapp/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ptw.fantasyleagueapp.activity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.ptw.fantasyleagueapp.R
