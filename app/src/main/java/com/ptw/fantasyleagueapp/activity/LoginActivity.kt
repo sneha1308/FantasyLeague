85348d74f4d62914665823f4f60aa95215f1ef1e
sridharG-PTW <sridhar.gonda@ptw.com>
2020-02-04 13:20:47 +0530

    Added the google+ signin

--- app/src/main/java/com/ptw/fantasyleagueapp/activity/LoginActivity.kt
+++ app/src/main/java/com/ptw/fantasyleagueapp/activity/LoginActivity.kt
@@ -4,51 +4,73 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.facebook.AccessToken
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
-import com.facebook.FacebookSdk
-import com.facebook.GraphRequest
-import com.facebook.appevents.AppEventsLogger
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.ptw.fantasyleagueapp.R
 import kotlinx.android.synthetic.main.activity_login.*
-import org.json.JSONException
 import java.util.*
 
 
 class LoginActivity : AppCompatActivity() {
-    val callbackManager = CallbackManager.Factory.create();
-    val TAG = LoginActivity::class.java.name
+    private val callbackManager = CallbackManager.Factory.create();
+    private val TAG = LoginActivity::class.java.name
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("963494167490-pn80ioe46dirnelsa66np7c3h5ngi277.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        val account = GoogleSignIn.getLastSignedInAccount(this)
         layFacebookLogin.setOnClickListener {
             LoginManager.getInstance()
                 .logInWithReadPermissions(this, Arrays.asList("email", "public_profile"))
             LoginManager.getInstance().logInWithPublishPermissions(this, Arrays.asList("publish_actions"))
             LoginManager.getInstance().registerCallback(callbackManager,
                 object : FacebookCallback<LoginResult?> {
-                    override fun onSuccess(loginResult: LoginResult?) { // App code
-                        Log.e(TAG, "${loginResult.toString()}")
+                    override fun onSuccess(loginResult: LoginResult?) {
+                        Log.e(TAG, "Facebook Token ${loginResult!!.accessToken}")
                     }
 
-                    override fun onCancel() { // App code
+                    override fun onCancel() {
                         Log.e(TAG, "onCancel")
                     }
 
-                    override fun onError(exception: FacebookException) { // App code
+                    override fun onError(exception: FacebookException) {
                         Log.e(TAG, "Exception $exception \n\n")
                         exception.printStackTrace()
-
                     }
                 })
         }
         layGoogle.setOnClickListener {
-            openAbout()
+            signIn();
+//            openAbout()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            Log.e(TAG, "Gmail Token ${account!!.idToken}")
+        } catch (e: ApiException) {
+            e.printStackTrace()
         }
     }
 
@@ -58,24 +80,12 @@ class LoginActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    private fun getUserProfile(currentAccessToken: AccessToken) {
-        val request = GraphRequest.newMeRequest(
-            currentAccessToken
-        ) { `object`, response ->
-            Log.d("TAG", `object`.toString())
-            try {
-                openAbout()
-            } catch (e: JSONException) {
-                e.printStackTrace()
-            }
+        if (requestCode === RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data)
         }
-        val parameters = Bundle()
-        parameters.putString("fields", "first_name,last_name,email,id")
-        request.parameters = parameters
-        request.executeAsync()
     }
 }
